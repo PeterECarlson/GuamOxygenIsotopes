db2cdca0da6deef2f4135758931492c39ad5e448
PeterECarlson <40280763+PeterECarlson@users.noreply.github.com>
2020-02-15 17:11:33 -0600

    Rename EVAPORATION.m to ISOLUTION_MOD/EVAPORATION.m

--- ISOLUTION_MOD/EVAPORATION.m
+++ ISOLUTION_MOD/EVAPORATION.m
@@ -3,10 +3,10 @@ function output = EVAPORATION(TC, h, V)
 
 T = 273.15 + TC;                                                                                %absolute temperature (K)
 E = exp(-6094.4642/T + 21.1249952 - 2.724552e-2*T + 1.6853396e-5*T^2 + 2.4575506*log(T));       %D. Sonntag (1982) [Pa]
-E = E/1.3332e2;                                                                                 %Pascal->Torr Stöcker 2005 [mmHg ]
-A = pi*0.017841241^2;                                                                           %Oberfläche einer Box mit Volumen von 0.1ml und einer Schichtdicke von 0.1mm
+E = E/1.3332e2;                                                                                 %Pascal->Torr StÃ¶cker 2005 [mmHg ]
+A = pi*0.017841241^2;                                                                           %OberflÃ¤che einer Box mit Volumen von 0.1ml und einer Schichtdicke von 0.1mm
 
-%eva = A * (0.002198 + 0.0398*0^0.5756) * E/1.3332e2 * (1-h)*1e6/3600; %µl/s (Bansal1998)
+%eva = A * (0.002198 + 0.0398*0^0.5756) * E/1.3332e2 * (1-h)*1e6/3600; %Âµl/s (Bansal1998)
 eva = A * (0.002198 + 0.0398*V^0.5756) * E * (1-h)*1000/3600/18; %mol/s (Bansal1998)
 
 %Output: evaporationrate (mol/s)
